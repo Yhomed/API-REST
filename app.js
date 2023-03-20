40c72a886c68e55fdf2490cf3edd626c81afabcc
Yhomed <yhomedurango@gmail.com>
2023-03-19 22:17:28 -0500

    rutas y api cancion y genero

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// rutas api 
+const cancionAPIRoutes = require('./routes/api/cancionAPIRoutes');
+const generoAPIRoutes = require('./routes/api/generoAPIRoutes');
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// endpoint predeterminado de api para canciones
+app.use('/canciones', cancionAPIRoutes);
+// endpoint predeterminado de api para generos
+app.use('/generos', generoAPIRoutes);
+
+app.listen(process.env.PORT || 3000, () => console.log('server activo |°°|-3000'));
---
+++ database/models/Albunes.js
@@ -0,0 +1,4 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = "Album";
+
+    let cols = {
